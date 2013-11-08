0adbb5290b652489eecdd24d739ef30934c1e9c4
unknown <1156546473@qq.com>
2013-11-08 23:32:53 +0800

    write the Dgls constructor

---
+++ Dgls/Dgls.js
@@ -0,0 +1,10 @@
+(function(window){
+    function Dgls(tagName, tagAttr, single){
+        this._name = tagName;
+        this._attr = tagAttr;
+        this._single = single;
+        this._brother = [];
+        this._children = [];
+    }
+
+})(window);
